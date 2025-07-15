9e097c6b458dbd1cc70fd946cf23705dc01feedb
Algus Dark <algus.dark@gmail.com>
2025-07-15 14:11:20 +0900

    test: verify pre-commit hook functionality

--- src/_meta.lua
+++ src/_meta.lua
@@ -4,6 +4,6 @@ return {
     name = 'miniflux',
     fullname = _('Miniflux'),
     description = _([[Read RSS entries from your Miniflux server.]]),
-    version = '0.0.2',
+    version = '0.0.1',
     author = 'Algus Dark',
 }
